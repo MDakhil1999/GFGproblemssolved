f66721ba7af3f97a6eb556eb4a9db12b5699655a
Akhil M D <42731608+MDakhil1999@users.noreply.github.com>
2021-05-15 17:32:57 +0530

    15may2021

---
+++ Leetcode/300. Longest Increasing Subsequence.py
@@ -0,0 +1,8 @@
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        lis = [1 for i in range(len(nums))]
+        for i in range(1,len(nums)):
+            for j in range(0,i):
+                if nums[i] > nums[j]:
+                    lis[i] = max(lis[i],lis[j]+1)
+        return max(lis)
